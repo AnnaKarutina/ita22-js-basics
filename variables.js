fac32c8c26b6e3f6e00b85ff1a4c18f6249dccd2
Anna Karutina <anna.karutina@khk.ee>
2023-09-08 11:32:30 +0300

    variables

---
+++ variables.js
@@ -0,0 +1,26 @@
+// let and const
+/*
+let name = 'Anna'
+console.log(name)
+const surname = 'Karutina'
+*/
+
+// array
+/*
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers)
+numbers.push(6)
+console.log(numbers)
+console.log(numbers[2])
+*/
+
+// objects
+const person = {
+	firstname: 'Anna',
+	lastname: 'Karutina'
+}
+console.log(person)
+console.log(person.firstname)
+console.log(person.lastname)
+person.department = 'IT Akadeemia'
+console.log(person)
