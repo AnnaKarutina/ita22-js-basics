77b7a3b5d98f037115e0936a09df4249058db852
Anna Karutina <anna.karutina@khk.ee>
2023-09-08 13:06:16 +0300

    math operators and Math package

---
+++ math.js
@@ -0,0 +1,21 @@
+const num1 = 100
+const num2 = 50
+let result
+
+result = num1 + num2
+result = num1 - num2
+result = num1 / num2
+result = num1 * num2
+result = num1 % num2
+
+result = Math.PI
+result = Math.round(2.5)
+result = Math.ceil(2.3)
+result = Math.floor(2.8)
+result = Math.sqrt(64)
+result = Math.abs(-5)
+result = Math.pow(8, 2)
+
+result = Math.floor(Math.random() * 11)
+
+console.log(result)
